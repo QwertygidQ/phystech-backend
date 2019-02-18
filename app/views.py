e0326a9077a026bc5c178f516bdd961d1fe55e2f
QwertygidQ <Qwertygid@gmail.com>
2019-02-18 15:19:54 +0300

    Sample API endpoint for checking

--- app/views.py
+++ app/views.py
@@ -1,7 +1,20 @@
 from . import app
 from flask_cors import cross_origin
+from flask import jsonify, request
 
 @app.route('/')
 @cross_origin()
 def index():
     return 'Phystech.Genesis Backend API'
+
+@app.route('/sample_api', methods=['POST'])
+@cross_origin()
+def sample():
+    if request.json:
+        return jsonify({
+            'status': 'OK'
+        })
+    
+    return jsonify({
+        'status': 'Error'
+    })
