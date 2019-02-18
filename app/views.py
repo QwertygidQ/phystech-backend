d6a4f46fe3e0e469dedce63ab2c38061b07db718
QwertygidQ <Qwertygid@gmail.com>
2019-02-18 16:06:26 +0300

    Removed unnecessary decorators

--- app/views.py
+++ app/views.py
@@ -1,14 +1,11 @@
 from . import app
-from flask_cors import cross_origin
 from flask import jsonify, request
 
 @app.route('/')
-@cross_origin()
 def index():
     return 'Phystech.Genesis Backend API'
 
 @app.route('/sample_api', methods=['POST'])
-@cross_origin()
 def sample():
     if request.json:
         return jsonify({
